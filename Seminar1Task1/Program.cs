66f4f856323705102567ff19404dee4199bfd4f8
Gamzat <gamzat87@gmail.com>
2023-10-18 22:24:10 +0530

    Comment added

--- Seminar1Task1/Program.cs
+++ Seminar1Task1/Program.cs
@@ -4,3 +4,31 @@
 // Например:
 // 3 -> Среда
 // 5 -> Пятница
+
+// System.Console.WriteLine("enter 2 numbers");
+// int num1 = int.Parse(Console.ReadLine());
+// int num2 = int.Parse(Console.ReadLine());
+// int x = num2 * num2;
+
+// if(num1 == x)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine ("NO");
+// }
+
+
+// Console.Write("Please write a day ");
+// int Day = int.Parse(Console.ReadLine()??"0");
+// string[] DayOfWeek = new string[7];
+// DayOfWeek[0] = "Monday";
+// DayOfWeek[1] = "Tuesday";
+// DayOfWeek[2] = "Wednsday";
+// DayOfWeek[3] = "Thursday";
+// DayOfWeek[4] = "Friday";
+// DayOfWeek[5] = "Saturday";
+// DayOfWeek[6] = "Sunday";
+
+// Console.WriteLine(DayOfWeek[Day-1]);
