494a169f79e4a31146f71c6ff501721f68c9f2a1
Gamzat <gamzat87@gmail.com>
2023-10-18 20:13:35 +0530

    Zadacha no1

--- Seminar1Task0/Program.cs
+++ Seminar1Task0/Program.cs
@@ -3,19 +3,39 @@
 // число и выдаёт его квадрат (число умноженное на
 // само себя).
 
-Console.WriteLine("Please enter number");
-// считаем данные с консоли 
-string? inputNum = Console.ReadLine();
- // ??"0";
-// проверям, чтобы данные были не пустыми
-if(inputNum!=null)
-{
-    //парсим выделенное число 
-    int num = int.Parse(inputNum);
-    // находим квадрат числа
-    int result = num*num;
-    // выводим данные в косоль
-    Console.WriteLine(result);
+// Console.WriteLine("Please enter number");
+// // считаем данные с консоли 
+// string? inputNum = Console.ReadLine();
+//  // ??"0";
+// // проверям, чтобы данные были не пустыми
+// if(inputNum!=null)
+// {
+//     //парсим выделенное число 
+//     int num = int.Parse(inputNum);
+//     // находим квадрат числа
+//     int result = num*num;
+//     // выводим данные в косоль
+//     Console.WriteLine(result);
 
-}
-Console.WriteLine("квадрат числа: "+(int)Math.Pow(int.Parse(inputNum),2));
+// }
+// Console.WriteLine("квадрат числа: "+(int)Math.Pow(int.Parse(inputNum),2));
+
+// Задача №1
+// Напишите программу, которая на вход принимает
+// два числа и проверяет, является ли первое число
+// квадратом второго.
+
+// Вводим два числа
+// int NumA = int.Parse(Console.ReadLine()??"0");
+// int NumB = int.Parse(Console.ReadLine()??"0");
+
+// // Проверяем число на квадрат и задаем если квадрат выводим да 
+// if(NumA*NumA == NumB)
+// {
+//     Console.WriteLine("Yes");
+// }
+// // условие если нет то выводим как нет
+// else
+//     {
+//         Console.WriteLine("No");
+//     }
