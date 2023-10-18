ea25428f2639afd7b6d9f5d2fd05a4751da2f7be
Gamzat <gamzat87@gmail.com>
2023-10-18 22:47:20 +0530

    solution from sem1 task0,1,3,5

---
+++ Seminar1Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Console.WriteLine("Enter Day of Week");
+// int dayNum = int.Parse(Console.ReadLine());
+// string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayofWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(dayNum-1));
+// Console.WriteLine(outDayOfWeek);
+
+
+// Задача №5
+// Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Например:
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+// int numN = int.Parse(Console.ReadLine()??"0");
+// int evenNumN = (-1)*numN;
+// while(evenNumN < numN)
+// {
+//     Console.Write(evenNumN+",");
+//     evenNumN++;
+// }
+// Console.WriteLine(numN);
+
+
---
+++ Seminar1Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numN = int.Parse(Console.ReadLine()??"0");
+if(numN<0) numN = -numN;
+for(int i=  -numN; i <= numN; i++)
+{
+    Console.Write(i);
+    Console.Write(",");
+}
